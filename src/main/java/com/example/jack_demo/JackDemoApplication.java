b835d7a63a4be0437c9781645ab24ea1bc5eea69
xiaowanzi-p <1194106705@qq.com>
2021-06-21 13:15:10 +0800

    first commit

---
+++ src/main/java/com/example/jack_demo/JackDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jack_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JackDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jack_demo/JackDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jack_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JackDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
